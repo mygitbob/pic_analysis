407d61812ce5266d32d42d9811a19626f267390b
mygitbob <107040418+mygitbob@users.noreply.github.com>
2023-05-03 19:05:40 +0200

    Update extract_exif.py
    Add alternative method to read exif data

--- extract_exif.py
+++ extract_exif.py
@@ -17,3 +17,17 @@ for file in os.listdir(path):
         except:
             print("Keine EXIF-Informationen für " + file)
 
+# Alternative            
+from PIL import Image # heisst die lib pillow
+import piexif
+import pickle
+
+img = Image.open('Image.png')
+
+exif_dict = img.info.get("exif") # returns None if exif key does not exist
+if exif_dict:
+  exif_data = piexif.load(exif_dict)
+  raw = exif_data['Exif'][piexif.ExifIFD.MakerNote]
+  tags = pickle.loads(raw)
+  print(tags)
+
